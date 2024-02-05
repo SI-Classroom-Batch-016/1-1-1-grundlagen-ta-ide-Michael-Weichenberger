727de2f4ccee84412eee858dafc85e8f26adc45a
Michael-Weichenberger <145458126+Michael-Weichenberger@users.noreply.github.com>
2024-02-05 16:02:38 +0100

    Aufgabe bearbeitet

--- src/main/kotlin/02_FachbegriffeVerstehen.kt
+++ src/main/kotlin/02_FachbegriffeVerstehen.kt
@@ -5,33 +5,33 @@ Aufgabe gibt es mindestens eine wahre Aussage, es sind aber auch mehrere wahre A
 (Setze ein x in die richtigen [ ] Klammern ) 
 
 **Der Code...**
-- [ ] besteht aus Anweisungen
-- [ ] soll später auf einem Gerät ausgeführt werden.
+- [x] besteht aus Anweisungen
+- [x] soll später auf einem Gerät ausgeführt werden.
 - [ ] besteht ausschließlich aus Zahlen.
 
 **Der Programmfluss...**
 - [ ] zeigt den Stromverbrauch deines Programms.
 - [ ] läuft normalerweise von unten nach oben ab.
-- [ ] beschreibt die Reihenfolge, in der die Anweisungen eines Programms ausgeführt werden.
+- [x] beschreibt die Reihenfolge, in der die Anweisungen eines Programms ausgeführt werden.
 
 **Die IDE...**
 - [ ] ist ein Programm, welches beim Erstellen von Programmen hilft.
 - [ ] steht für Identical Durability Exception.
-- [ ] ist eine Programmierumgebung.
+- [x] ist eine Programmierumgebung.
 
 **Syntax...**
-- [ ] ist die genaue Schreibweise einer Anwendung.
-- [ ] ist wichtig für die Lauffähigkeit eines Programms.
+- [x] ist die genaue Schreibweise einer Anwendung.
+- [x] ist wichtig für die Lauffähigkeit eines Programms.
 - [ ] ist in jeder Programmiersprache verschieden.
 
 **Kompilieren...**
 - [ ] kontrolliert, ob deine Texte Rechtschreibfehler enthalten.
-- [ ] kontrolliert, ob die Syntax eines Programms stimmt.
+- [x] kontrolliert, ob die Syntax eines Programms stimmt.
 - [ ] wird bei einem Problem durch einen Kompilierfehler unterbrochen.
 
 **Ausführen...**
 - [ ] lässt sich ein Programm nie.
-- [ ] ist der Prozess, in dem das Programm auf einem Gerät gestartet wird.
+- [x] ist der Prozess, in dem das Programm auf einem Gerät gestartet wird.
 - [ ] heißt, das Programm zu beenden.
 
 
@@ -41,4 +41,9 @@ Platz um deine Antwort für die Codevorhersage zu notieren:
 
 
 
-*/ 
+
+Hello World!
+Heute ist ein schöner Tag!
+Sehr gut! Ihr habt es fast geschafft.
+Jetzt könnt ihr schon eure ersten Zeilen Code lesen.*/
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+println("Hallo, wie heißt du? (confirm with Enter)")
+    var name = readln()
+    println("Hallo $name!")
+
+    println("Wie alt bist du? (confirm with Enter)")
+    var alter = readln()
+    println("Wow! Bereits $alter Jahre?!")
+}
